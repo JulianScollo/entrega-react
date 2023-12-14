9e2232e331f9a10d0dd111a307897334f0a5a994
julian scollo git <julianscollogit@gmail.com>
2023-12-14 19:03:09 -0300

    tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import ItemListContainer from './componentes/ItemListContainer'
 function App() {
   
   return (
-    <>
+      <div id='app'>
       <Navbar />
       <ItemListContainer producto="Futuros productos"/>
-    </>
-  )
+      </div>
+  ) 
 }
 
 export default App
